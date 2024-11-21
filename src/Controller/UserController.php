46544950d62b30a02cdf4a33c9ee1c0c6f1c4644
Deltah3 <c.gentili@it-students.fr>
2024-11-21 15:45:38 +0100

    merge main

---
+++ src/Controller/UserController.php
@@ -0,0 +1,81 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Service\UserService;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Transport\TransportInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+
+class UserController extends AbstractController
+{
+    #[Route('/register', name: 'app_register', methods: ['GET', 'POST'])]
+    public function register(
+        Request $request,
+        EntityManagerInterface $em,
+        UserService $userService,
+        TransportInterface $transport
+    ): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            if(!$userService->isPasswordSecured($user->getPassword())) {
+                $this->addFlash('error', "Le mot de passe n'est pas sécurisé");
+                return $this->redirectToRoute('app_register');
+            }
+
+            $user = $userService->hashPassword($user);
+            $user->setCreatedAt(new \DateTimeImmutable());
+
+            $request->getSession()->set('currentUser', $user);
+
+            try {
+                $em->persist($user);
+                $em->flush();
+
+                $message = new Email();
+                $message
+                    ->to($user->getEmail())
+                    ->from('noreply@ntcm.fr')
+                    ->text(
+                        sprintf(
+                            'Vous avez créé votre compte ! Pour accéder à votre profil, cliquez sur ce lien : %s',
+                            $this->generateUrl('app_profile', ['id' => $user->getId()], UrlGeneratorInterface::ABSOLUTE_URL)
+                        )
+                    );
+
+                $transport->send($message);
+
+                $this->addFlash('success', 'Vous avez été enregistré');
+            } catch (UniqueConstraintViolationException $e) {
+                $this->addFlash('error', 'Cet email est déjà utilisé');
+            }
+
+            // TODO: redirect to app_profile
+            return $this->redirectToRoute('app_register');
+        }
+
+        return $this->render('user/registration.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/profile', name: 'app_profile')]
+    public function profile(): Response
+    {
+        return $this->render('user/profile.html.twig', [
+        
+        ]);
+    }
+}
